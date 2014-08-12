734a492727b9cd6765e97a73bff3732c4a678174
benbiron <benbiron@gmail.com>
2014-08-12 02:03:54 -0400

    Change demo to show most supported features

--- rpi-led-text/main.cc
+++ rpi-led-text/main.cc
@@ -15,29 +15,93 @@ using std::max;
 
 int main(int argc, char *argv[]) {
   int demo = 0;
+  int sleep_time = 3;
   LEDDisplay* leddisp = new LEDDisplay();
 
   if (argc > 1) {
     demo = atoi(argv[1]);
+    if (argc > 2) {
+      sleep_time = atoi(argv[2]);
+      if (sleep_time <= 0) {
+        sleep_time = 3;
+      }
+    }
   }
   fprintf(stderr, "Using demo %d\n", demo);
 
   switch (demo) {
   case 0:
-    leddisp->DisplayString("DSA", LEDDisplay::kLeft, LEDDisplay::kNoScroll, LEDDisplay::kVariable);
+    while (1) {
+      unsigned timer_ms;
+      leddisp->SetRGB(0, 0xff, 0xff);
+
+      printf("Left scroll\n");
+      leddisp->DisplayString("Shawn & Anne & ", LEDDisplay::kLeft, LEDDisplay::kLeftScroll, LEDDisplay::kVariable);
+      sleep(sleep_time);
+
+      printf("Up scroll\n");
+      leddisp->DisplayString("Shawn & Anne & ", LEDDisplay::kLeft, LEDDisplay::kUpScroll, LEDDisplay::kVariable);
+      sleep(sleep_time);
+
+      printf("Timer\n");
+      leddisp->SetRGB(0, 0, 0xff);
+      timer_ms = 825000;
+      while (timer_ms < 830000) {
+        leddisp->DisplayTimer(timer_ms);
+        timer_ms += 100;
+        usleep(100000);
+      }
+
+      printf("Pictograms\n");
+      leddisp->SetRGB(0xff, 0, 0);
+      leddisp->DisplayX(LEDDisplay::kDupeCenter, LEDDisplay::kDownScroll);
+      sleep(2);
+      leddisp->SetRGB(0, 0xff, 0);
+      leddisp->DisplayCheckmark(LEDDisplay::kDupeCenter, LEDDisplay::kUpScroll);
+      sleep(2);
+      leddisp->SetRGB(0xff, 0, 0);
+      leddisp->DisplayHeart(LEDDisplay::kDupeCenter, LEDDisplay::kRightScroll);
+      sleep(2);
+      leddisp->DisplayHeart(LEDDisplay::kDupeCenter, LEDDisplay::kLeftScroll);
+      sleep(2);
+      leddisp->DisplayX(LEDDisplay::kDupeRight, LEDDisplay::kNoScroll);
+      sleep(2);
+      leddisp->DisplayX(LEDDisplay::kLeft, LEDDisplay::kNoScroll);
+      sleep(2);
+      leddisp->DisplayX(LEDDisplay::kCenter, LEDDisplay::kNoScroll);
+      sleep(2);
+      leddisp->SetRGB(0, 0xff, 0xff);
+
+      printf("Display fill\n");
+      leddisp->SetRGB(0xff, 0, 0);
+      leddisp->DisplayFill();
+      sleep(1);
+      leddisp->SetRGB(0, 0xff, 0);
+      leddisp->DisplayFill();
+      sleep(1);
+      leddisp->SetRGB(0, 0, 0xff);
+      leddisp->DisplayFill();
+      sleep(1);
+      leddisp->SetRGB(0xff, 0xff, 0);
+      leddisp->DisplayFill();
+      sleep(1);
+      leddisp->SetRGB(0xff, 0, 0xff);
+      leddisp->DisplayFill();
+      sleep(1);
+      leddisp->SetRGB(0, 0xff, 0xff);
+      leddisp->DisplayFill();
+      sleep(1);
+    }
     break;
+
+  case 1:
+    delete leddisp;
+    return 0;
 
   default:
     printf("Unknown demo: %d\n", demo);
     break;
   }
-
-  // Things are set up. Just wait for <RETURN> to be pressed.
-  printf("Press <RETURN> to exit and reset LEDs\n");
-  getchar();
-
-  // Stopping threads and wait for them to join.
-  delete leddisp;
 
   return 0;
 }
