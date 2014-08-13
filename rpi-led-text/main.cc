253602783d56c1f88881e3016dbc5a46825c7be6
benbiron <benbiron@gmail.com>
2014-08-13 01:03:22 -0400

    Add canvas API demo that doubles as a pictographic font demo

--- rpi-led-text/main.cc
+++ rpi-led-text/main.cc
@@ -1,6 +1,7 @@
 #include "thread.h"
 #include "led-matrix.h"
 #include "led-display.h"
+#include "text.h"
 
 #include <assert.h>
 #include <unistd.h>
@@ -13,6 +14,110 @@
 using std::min;
 using std::max;
 
+void leddisplay_demo(LEDDisplay* leddisp)
+{
+  while (1) {
+    unsigned timer_ms;
+    leddisp->SetRGB(0, 0xff, 0xff);
+
+    printf("Left scroll\n");
+    leddisp->DisplayString("Shawn & Anne & ", LEDDisplay::kLeft, LEDDisplay::kLeftScroll, LEDDisplay::kVariable);
+    sleep(3);
+
+    printf("Up scroll\n");
+    leddisp->DisplayString("Shawn & Anne & ", LEDDisplay::kLeft, LEDDisplay::kUpScroll, LEDDisplay::kVariable);
+    sleep(3);
+
+    printf("Left\n");
+    leddisp->DisplayString("L", LEDDisplay::kLeft, LEDDisplay::kNoScroll, LEDDisplay::kVariable);
+    sleep(1);
+
+    printf("Right\n");
+    leddisp->DisplayString("R", LEDDisplay::kRight, LEDDisplay::kNoScroll, LEDDisplay::kVariable);
+    sleep(1);
+
+    printf("Center\n");
+    leddisp->DisplayString("C", LEDDisplay::kCenter, LEDDisplay::kNoScroll, LEDDisplay::kVariable);
+    sleep(1);
+
+    printf("Timer\n");
+    leddisp->SetRGB(0, 0, 0xff);
+    timer_ms = 825000;
+    while (timer_ms < 830000) {
+      leddisp->DisplayTimer(timer_ms);
+      timer_ms += 100;
+      usleep(100000);
+    }
+
+    printf("Pictograms\n");
+    leddisp->SetRGB(0xff, 0, 0);
+    leddisp->DisplayX(LEDDisplay::kDupeCenter, LEDDisplay::kDownScroll);
+    sleep(2);
+    leddisp->SetRGB(0, 0xff, 0);
+    leddisp->DisplayCheckmark(LEDDisplay::kDupeCenter, LEDDisplay::kUpScroll);
+    sleep(2);
+    leddisp->SetRGB(0xff, 0, 0);
+    leddisp->DisplayHeart(LEDDisplay::kDupeCenter, LEDDisplay::kRightScroll);
+    sleep(2);
+    leddisp->DisplayHeart(LEDDisplay::kDupeCenter, LEDDisplay::kLeftScroll);
+    sleep(2);
+    leddisp->DisplayX(LEDDisplay::kDupeRight, LEDDisplay::kNoScroll);
+    sleep(2);
+    leddisp->DisplayX(LEDDisplay::kLeft, LEDDisplay::kNoScroll);
+    sleep(2);
+    leddisp->DisplayX(LEDDisplay::kCenter, LEDDisplay::kNoScroll);
+    sleep(2);
+    leddisp->SetRGB(0, 0xff, 0);
+    leddisp->DisplayCheckmark(LEDDisplay::kLeft, LEDDisplay::kRightScroll);
+    sleep(2);
+
+    printf("Display fill\n");
+    leddisp->SetRGB(0xff, 0, 0);
+    leddisp->DisplayFill();
+    sleep(1);
+    leddisp->SetRGB(0, 0xff, 0);
+    leddisp->DisplayFill();
+    sleep(1);
+    leddisp->SetRGB(0, 0, 0xff);
+    leddisp->DisplayFill();
+    sleep(1);
+    leddisp->SetRGB(0xff, 0xff, 0);
+    leddisp->DisplayFill();
+    sleep(1);
+    leddisp->SetRGB(0xff, 0, 0xff);
+    leddisp->DisplayFill();
+    sleep(1);
+    leddisp->SetRGB(0, 0xff, 0xff);
+    leddisp->DisplayFill();
+    sleep(1);
+
+    printf("Display clear\n");
+    leddisp->DisplayClear();
+    sleep(1);
+  }
+}
+
+void canvas_picto_demo(LEDDisplay* leddisp)
+{
+  Font pictofont("fonts/modernpics.otf", 16);
+  Font font("fonts/Oswald-Bold.ttf", 15);
+  char index_str[8];
+  char c;
+  int i;
+  RGBCanvas* canvas;
+
+  while (1) {
+    for (c=' ', i=0; c <= '}'; c++, i++) {
+      snprintf(index_str, sizeof(index_str), "%d", c);
+      canvas = new RGBCanvas(leddisp->GetWidth(), leddisp->GetHeight());
+      font.PaintString(index_str, canvas, 1, leddisp->GetHeight() - 2, 0xff, 0, 0);
+      pictofont.PaintChar(c, canvas, leddisp->GetWidth() / 2 + 1, leddisp->GetHeight() - 2, 0, 0xff, 0xff);
+      leddisp->SwapCanvas(canvas, LEDDisplay::kLeft, LEDDisplay::kNoScroll);
+      usleep(700000);
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
   int demo = 0;
   LEDDisplay* leddisp = new LEDDisplay();
@@ -24,84 +129,14 @@ int main(int argc, char *argv[]) {
 
   switch (demo) {
   case 0:
-    while (1) {
-      unsigned timer_ms;
-      leddisp->SetRGB(0, 0xff, 0xff);
-
-      printf("Left scroll\n");
-      leddisp->DisplayString("Shawn & Anne & ", LEDDisplay::kLeft, LEDDisplay::kLeftScroll, LEDDisplay::kVariable);
-      sleep(3);
-
-      printf("Up scroll\n");
-      leddisp->DisplayString("Shawn & Anne & ", LEDDisplay::kLeft, LEDDisplay::kUpScroll, LEDDisplay::kVariable);
-      sleep(3);
-
-      printf("Left\n");
-      leddisp->DisplayString("L", LEDDisplay::kLeft, LEDDisplay::kNoScroll, LEDDisplay::kVariable);
-      sleep(1);
-
-      printf("Right\n");
-      leddisp->DisplayString("R", LEDDisplay::kRight, LEDDisplay::kNoScroll, LEDDisplay::kVariable);
-      sleep(1);
-
-      printf("Center\n");
-      leddisp->DisplayString("C", LEDDisplay::kCenter, LEDDisplay::kNoScroll, LEDDisplay::kVariable);
-      sleep(1);
-
-      printf("Timer\n");
-      leddisp->SetRGB(0, 0, 0xff);
-      timer_ms = 825000;
-      while (timer_ms < 830000) {
-        leddisp->DisplayTimer(timer_ms);
-        timer_ms += 100;
-        usleep(100000);
-      }
-
-      printf("Pictograms\n");
-      leddisp->SetRGB(0xff, 0, 0);
-      leddisp->DisplayX(LEDDisplay::kDupeCenter, LEDDisplay::kDownScroll);
-      sleep(2);
-      leddisp->SetRGB(0, 0xff, 0);
-      leddisp->DisplayCheckmark(LEDDisplay::kDupeCenter, LEDDisplay::kUpScroll);
-      sleep(2);
-      leddisp->SetRGB(0xff, 0, 0);
-      leddisp->DisplayHeart(LEDDisplay::kDupeCenter, LEDDisplay::kRightScroll);
-      sleep(2);
-      leddisp->DisplayHeart(LEDDisplay::kDupeCenter, LEDDisplay::kLeftScroll);
-      sleep(2);
-      leddisp->DisplayX(LEDDisplay::kDupeRight, LEDDisplay::kNoScroll);
-      sleep(2);
-      leddisp->DisplayX(LEDDisplay::kLeft, LEDDisplay::kNoScroll);
-      sleep(2);
-      leddisp->DisplayX(LEDDisplay::kCenter, LEDDisplay::kNoScroll);
-      sleep(2);
-      leddisp->SetRGB(0, 0xff, 0);
-      leddisp->DisplayCheckmark(LEDDisplay::kLeft, LEDDisplay::kRightScroll);
-      sleep(2);
-
-      printf("Display fill\n");
-      leddisp->SetRGB(0xff, 0, 0);
-      leddisp->DisplayFill();
-      sleep(1);
-      leddisp->SetRGB(0, 0xff, 0);
-      leddisp->DisplayFill();
-      sleep(1);
-      leddisp->SetRGB(0, 0, 0xff);
-      leddisp->DisplayFill();
-      sleep(1);
-      leddisp->SetRGB(0xff, 0xff, 0);
-      leddisp->DisplayFill();
-      sleep(1);
-      leddisp->SetRGB(0xff, 0, 0xff);
-      leddisp->DisplayFill();
-      sleep(1);
-      leddisp->SetRGB(0, 0xff, 0xff);
-      leddisp->DisplayFill();
-      sleep(1);
-    }
+    leddisplay_demo(leddisp);
     break;
 
   case 1:
+    canvas_picto_demo(leddisp);
+    break;
+
+  case 2:
     delete leddisp;
     return 0;
 
